8457c791428557e5d9cd23373833c58bfd5db1e2
Souibgui Mohamed Amine <mohamedamine.souibgui@esprit.tn>
2025-03-31 17:17:20 +0100

    crud Evenement

---
+++ src/Form/EvenementType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Evenement;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EvenementType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom')
+            ->add('description')
+            ->add('date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('lieu')
+            ->add('statut')
+            ->add('capacite_max')
+            ->add('image')
+            ->add('type')
+            ->add('utilisateur', EntityType::class, [
+                'class' => Utilisateur::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Evenement::class,
+        ]);
+    }
+}
